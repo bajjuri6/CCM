c89ba48a0f9baf6647d124ec007cc02d30259b23
Pallav Bajjuri <bajjuri@viveninfomedia.com>
2016-05-28 19:43:23 +0530

    Added logout and cleaned up the rest.

--- application/layout/navigationHeader.php
+++ application/layout/navigationHeader.php
@@ -1,29 +1,13 @@
-<md-toolbar>
+<md-toolbar id="mainHeader">
     <div class="md-toolbar-tools">
         <div layout layout-align="space-between center" flex>
-            <div><h1>ccn logo</h1></div>
+            <div><a href="/"><img src="/public/Images/ccm_logo.png" /></a></div>
             <div layout layout-align="center center">
-                <md-button class="md-raised  md-accent" ui-sref="leads">All Leads</md-button>
-                <md-menu-bar layout layout-xs="row">
-                    <md-menu>
-                        <button  ng-click="$mdOpenMenu()">
-                            Manage
-                        </button>
-                        <md-menu-content>
-                            <md-menu-item><md-button href="/employee">EMPLOYEES</md-button></md-menu-item>
-                            <md-menu-item><md-button href="/rcp">RCP</md-button></md-menu-item>
-                        </md-menu-content>
-                    </md-menu>
-                    <md-menu>
-                        <button  ng-click="$mdOpenMenu()">
-                            UserName
-                        </button>
-                        <md-menu-content>
-                            <md-menu-item><md-button>profile</md-button></md-menu-item>
-                            <md-menu-item><md-button href="logout.php">Logout</md-button></md-menu-item>
-                        </md-menu-content>
-                    </md-menu>
-                </md-menu-bar>
+                
+              <md-button class="md-button md-accent md-hue-1" href="/leads">All Leads</md-button>
+              <md-button class="md-button md-accent md-hue-1" href="/users">Users</md-button>
+              <md-button class="md-button md-accent md-hue-1" href="/logout">Logout</md-button>
+              
             </div> 
         </div>
     </div>
