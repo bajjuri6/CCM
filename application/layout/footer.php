b3f844be4206eed5330e649b513b6f73dc58aaf3
Pallav Bajjuri <bajjuri@viveninfomedia.com>
2016-05-28 14:54:19 +0530

    Removing the footer as we don't need it here.

--- application/layout/footer.php
+++ application/layout/footer.php
@@ -1,9 +1,9 @@
-<md-toolbar>
+<!--<md-toolbar>
     <div class="md-toolbar-tools">
         <div layout layout-align="center center" flex>
             Footer 
         </div>
     </div>
-</md-toolbar>
+</md-toolbar>-->
 </body>
 </html>
