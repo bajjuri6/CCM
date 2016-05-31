8eb80f10dd5dbd721cfc8d88cc7dca8b25e78c2e
sandeepreddyponagandla <sandeep.ponagandla@viveninfomedia.com>
2016-05-31 13:08:15 +0530

    Added 'export to excel' functionality

--- application/views/index/leads.php
+++ application/views/index/leads.php
@@ -1,42 +1,40 @@
 <div class="wrapper" layout="column" ng-controller="LeadController" fix-nav ng-init="setupLeads()" >
-    <div layout="column" layout-align="center center">
-        <div>
-            <div class="md-padding"></div>
-            <md-button class="md-raised md-hue-2 md-primary" ng-click="addNewLead($event)">New Lead</md-button>
-        </div>
-
-        <table flex>
-            <tr>
-                <th>Customer Name</th>
-                <th>Phone Number</th>
-                <th>Address</th>
-                <th>Occupation</th>
-                <th>Occupation detail</th>
-                <th cols="2">Business detail</th>
-                <th>PAN Number</th>
-                <th>Aadhaar Number</th>
-                <th>Status</th>
-            </tr>
-
-            <tr ng-repeat='lead in leads' id="{{lead.lid}}">
-                <td ng-bind="lead.name"></td>
-                <td ng-bind="lead.phone"></td>
-                <td ng-bind="lead.addr"></td>
-                <td ng-bind="setOcpt(lead.occupation)"></td>
-                <td ng-bind="setSubOcpt(lead.occupation, lead.occupationdetail)"></td>
-                <td cols="2" ccm-html="bldBizTxt($index)"></td>
-                <td ng-bind="lead.pan"></td>
-                <td ng-bind="lead.aadhaar"></td>
-                <td>
-                    <div ng-if="lead.sts == 0">
-                        <a href="#" class="_act-btn" ng-click="apprvLd(lead.lid, $index, $event)">Approve</a>
-                        <a href="#" class="_act-btn" ng-click="dltLd(lead.lid, $index, $event)">Reject</a>
-                    </div>
-                    <div ng-if="lead.sts != 0">
-                        <span class="_act-btn">{{lead.sts == 1 ? 'Approved' : 'Rejected'}}</span>
-                    </div>
-                </td>
-            </tr>
-        </table>
+    
+    <div layout layout-align="end center" style="margin-top : 32px;">
+        <md-button class="md-button md-hue-2 md-primary" ng-click="addNewLead($event)">New Lead</md-button>
+        <md-button class="md-button md-primary md-hue-2" ng-click="expToXl('#ldsTbl')">Export to Excel</md-button>
     </div>
+    <table flex id="ldsTbl">
+        <tr>
+            <th>Customer Name</th>
+            <th>Phone Number</th>
+            <th>Address</th>
+            <th>Occupation</th>
+            <th>Occupation detail</th>
+            <th cols="2">Business detail</th>
+            <th>PAN Number</th>
+            <th>Aadhaar Number</th>
+            <th>Status</th>
+        </tr>
+
+        <tr ng-repeat='lead in leads' id="{{lead.lid}}">
+            <td ng-bind="lead.name"></td>
+            <td ng-bind="lead.phone"></td>
+            <td ng-bind="lead.addr"></td>
+            <td ng-bind="setOcpt(lead.occupation)"></td>
+            <td ng-bind="setSubOcpt(lead.occupation, lead.occupationdetail)"></td>
+            <td cols="2" ccm-html="bldBizTxt($index)"></td>
+            <td ng-bind="lead.pan"></td>
+            <td ng-bind="lead.aadhaar"></td>
+            <td>
+                <div ng-if="lead.sts == 0">
+                    <a href="#" class="_act-btn" ng-click="apprvLd(lead.lid, $index, $event)">Approve</a>
+                    <a href="#" class="_act-btn" ng-click="dltLd(lead.lid, $index, $event)">Reject</a>
+                </div>
+                <div ng-if="lead.sts != 0">
+                    <span class="_act-btn">{{lead.sts == 1 ? 'Approved' : 'Rejected'}}</span>
+                </div>
+            </td>
+        </tr>
+    </table>
 </div>
--- public/home.js
+++ public/home.js
@@ -55,6 +55,7 @@ app.directive('fixNav', function () {
         }
     }
 });
+
 app.directive('ccmHtml', function () {
     return {
         restrict: 'AC',
@@ -71,6 +72,27 @@ app.directive('ccmHtml', function () {
         }
     };
 });
+app.factory('Excel', function ($window) {
+    var uri = 'data:application/vnd.ms-excel;base64,',
+            template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
+            base64 = function (s) {
+                return $window.btoa(unescape(encodeURIComponent(s)));
+            },
+            format = function (s, c) {
+                return s.replace(/{(\w+)}/g, function (m, p) {
+                    return c[p];
+                })
+            };
+    return {
+        tableToExcel: function (tableId, worksheetName) {
+            var table = $(tableId),
+                    ctx = {worksheet: worksheetName, table: table.html()},
+            href = uri + base64(format(template, ctx));
+            return href;
+        }
+    };
+});
+/* Controllers */
 app.controller('ToastController', ['$scope', '$mdToast',
     function ($scope, $mdToast) {
         var last = {
@@ -281,8 +303,8 @@ app.controller("UserController", ["$scope", "CCMAPI", "$controller", "manageUser
         };
     }]);
 
-app.controller("LeadController", ["$scope", "CCMAPI", "$controller", "manageLead",
-    function ($scope, CCMAPI, $controller, manageLead) {
+app.controller("LeadController", ["$scope", "CCMAPI", "$controller", "manageLead", "Excel", "$timeout",
+    function ($scope, CCMAPI, $controller, manageLead, Excel, $timeout) {
         var self = this;
         $controller('ToastController', {$scope: $scope});
         $controller('DialogController', {$scope: $scope});
@@ -377,7 +399,7 @@ app.controller("LeadController", ["$scope", "CCMAPI", "$controller", "manageLead
                     {
                         $scope.cust.sts = 0;
                         self.parScope.leads.push($scope.cust);
-                        window.scrollTo(0,document.body.scrollHeight);
+                        window.scrollTo(0, document.body.scrollHeight);
                         $scope.cancel();
                     }
                     else
@@ -406,6 +428,13 @@ app.controller("LeadController", ["$scope", "CCMAPI", "$controller", "manageLead
                     }
                 });
             });
+        };
+        
+        $scope.expToXl = function(tblId) {
+            $scope.exportHref = Excel.tableToExcel(tblId, 'sheet name');
+            $timeout(function(){
+              location.href = $scope.exportHref;  
+            },100);
         };
 
     }]);
