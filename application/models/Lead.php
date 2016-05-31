372345ba30a9a77b7bc3f314c688b336d19b0eb5
Satish <mukkojusatish@gmail.com>
2016-05-31 12:24:21 +0530

    adding userid into _tbl_lead_ref_id insted of _tbl_lead_added_by_ccmid

--- application/models/Lead.php
+++ application/models/Lead.php
@@ -7,9 +7,9 @@ class Lead{
     $db = $this -> getDB('r', '');
     $time = time();
     $id = md5($time.$_POST['name'].$phone);
-    if($db->query("INSERT INTO _table_leads_ccm VALUES(".$db->quote($id).", '', ".$db->quote($name).",
+    if($db->query("INSERT INTO _table_leads_ccm VALUES(".$db->quote($id).", ".$db->quote($_SESSION['usr_id']).", ".$db->quote($name).",
                ".$db->quote($addr).", ".$db->quote($phone).", ".$db->quote($occupation).", ".$db->quote($occupationdetail).",
-               ".$db->quote($biz).", ".$db->quote($income).", ".$db->quote($pan).", ".$db->quote($aadhaar).", 0, '', ".$db->quote($_SESSION['usr_id']).", ".$time.", '')")) {
+               ".$db->quote($biz).", ".$db->quote($income).", ".$db->quote($pan).", ".$db->quote($aadhaar).", 0, '', '', ".$time.", '')")) {
       return 1;
     }
     else {
@@ -24,7 +24,7 @@ class Lead{
                       . " _tbl_lead_cust_phone as phone, _tbl_lead_cust_occupation as occupation, "
                       . " _tbl_lead_cust_occupation_sub as occupationdetail, _tbl_lead_cust_biz as biz, "
                       . " _tbl_lead_cust_income as income, _tbl_lead_cust_pan as pan, _tbl_lead_cust_aadhaar as aadhaar, "
-                      . " _tbl_lead_status as sts, _tbl_lead_added_by_ccmid as user, _tbl_lead_added_on as time FROM _table_leads_ccm WHERE _tbl_lead_status=0 ORDER BY _tbl_lead_added_on LIMIT 0,20");
+                      . " _tbl_lead_status as sts, _tbl_lead_ref_id as user, _tbl_lead_added_on as time FROM _table_leads_ccm ORDER BY _tbl_lead_added_on LIMIT 0,20");
       $r = $tmp->fetchAll(PDO::FETCH_ASSOC);
       return '{"status": 1, "msg": '.json_encode($r).'}';
     }else {
@@ -35,7 +35,7 @@ class Lead{
     if(isset($_SESSION['usr_id']) && $_SESSION['usr_lvl'] >= 1){
       $leadId = $_POST['id'];
       $db = $this -> getDB('r', '');
-      if($db->query("UPDATE _table_leads_ccm SET _tbl_lead_status = 1 WHERE _tbl_lead_id=".$db->quote($leadId))){
+      if($db->query("UPDATE _table_leads_ccm SET _tbl_lead_status = 1, _tbl_lead_added_by_ccmid = ".$db->quote($_SESSION['usr_id'])." WHERE _tbl_lead_id=".$db->quote($leadId))){
         return '{"status": 1, "msg": "Lead approved successfully!!"}';
       }
       else {
@@ -50,7 +50,7 @@ class Lead{
     if(isset($_SESSION['usr_id']) && $_SESSION['usr_lvl'] >= 1){
       $leadId = $_POST['id'];
       $db = $this -> getDB('r', '');
-      if($db->query("UPDATE _table_leads_ccm SET _tbl_lead_status = -1 WHERE _tbl_lead_id=".$db->quote($leadId))){
+      if($db->query("UPDATE _table_leads_ccm SET _tbl_lead_status = -1, _tbl_lead_added_by_ccmid = ".$db->quote($_SESSION['usr_id'])." WHERE _tbl_lead_id=".$db->quote($leadId))){
         return '{"status": 1, "msg": "Lead deleted successfully!!"}';
       }
       else {
