f0b97b15a98145965845b3d4f1913f1f066c0487
Pallav Bajjuri <bajjuri@viveninfomedia.com>
2016-06-01 20:35:56 +0530

    Adding route to handle lead submissions from mobile

--- library/Bootstrap.php
+++ library/Bootstrap.php
@@ -51,6 +51,9 @@ class Bootstrap {
         } elseif ($url[0] == 'adld') {
             require APP_PATH . '/controllers/lead.php';
             echo (new leadController())->saveLead();
+        } elseif ($url[0] == 'madld') {
+            require APP_PATH . '/controllers/lead.php';
+            echo (new leadController())->saveLeadFromMobile();
         } elseif ($url[0] == 'gtlds') {
             require APP_PATH . '/models/Lead.php';
             echo (new Lead())->getLeads();
